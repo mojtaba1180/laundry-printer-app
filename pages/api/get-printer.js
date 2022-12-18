8efb4525635777bb1857e73403c91ef8931cf7ce
mohammad asghari <mohammadsgr8@gmail.com>
2022-12-18 12:47:37 -0800

    update api printer and add print api route

--- pages/api/get-printer.js
+++ pages/api/get-printer.js
@@ -1,14 +1,19 @@
 const nodePrinter = require('@thiagoelg/node-printer');
 
- const  handler =  async (req, res) => { 
-    const getPrinter = await nodePrinter.getPrinter();
-    let response = []
-    if(Array.isArray(getPrinter)){
-        response = getPrinter
-    }else{
-        response.push(getPrinter)
+const handler = async (req, res) => {
+    if (req.method === "GET") {
+        const getPrinter = nodePrinter.getPrinter();
+        console.log(req.method);
+        let response = []
+        if (Array.isArray(getPrinter)) {
+            response = getPrinter
+        } else {
+            response.push(getPrinter)
+        }
+        res.status(200).json(response)
+    } else {
+        res.status(403).json({ message: "for denied " })
     }
-    res.status(200).json(response)
 }
 export default handler
 
---
+++ pages/api/get-printers.js
@@ -0,0 +1,19 @@
+const nodePrinter = require('@thiagoelg/node-printer');
+
+ const  handler =  async (req, res) => { 
+    if(req.method === "GET"){
+        const getPrinter =  nodePrinter.getPrinters();
+        let response = []
+        if(Array.isArray(getPrinter)){
+            response = getPrinter
+        }else{
+            response.push(getPrinter)
+        }
+        res.status(200).json(response)
+    }else {
+        res.status(403).json("for denied")
+
+    }
+}
+export default handler
+
